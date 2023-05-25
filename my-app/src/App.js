62884c7f8420ade4576263a76dacefdf630b7b24
Oliver Smith <oliverliver.smith@minderacodeacademy.com>
2023-05-25 20:52:24 +0100

    Co-authored-by: zain3002ali@gmail.com <zain3002ali@gmail.com>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,46 @@
 import "./App.css";
 
+
+
 function App() {
   return (
     <div>
       <div className="NavigationBar">
+        <br></br>
+        <br></br> 
         <div className="SearchBars">
-          <input className= "search" defaultValue = "Search..."></input>
+          <input className= "search" placeholder = "Search..." defaultValue=""></input>
           <select id="search-field-choice">
+          <option value="" disabled selected>Select search field</option>
             <option value="itemId">ID number</option>
             <option value="itemName">Name</option>
             <option value="itemPrice">Price</option>
             <option value="itemCategory">Category</option>
         </select>
         </div>
+        <br></br> 
         <div className="EditBar">
-          <input defaultValue = "Item Name"></input>
-          <input defaultValue = "Price"></input>
+          <input className="name" placeholder = "enter item name here" defaultValue= ""></input>
+          <input className="price" placeholder = "enter price here" defaultValue=""></input>
           <select id="Categories">
-            <option value="blank">please select category</option>
+            <option value="" disabled selected>Please select category</option>
             <option value="food">Food and Drink</option>
             <option value="appliance">Appliances</option>
             <option value="stationary">Stationary</option>
             <option value="clothes">Clothing</option>
-            <option value="diy">DIY and repairs</option>
+            <option value="diy">DIY and Repairs</option>
             <option value="entertainment">Entertainment</option>
             <option value="garden">Gardening</option>
             <option value="cleaning">Cleaning Products</option>
-            <option value="Other">Other items</option>
+            <option value="toiletries">Toiletries</option>
+            <option value="other">Other Items</option>
         </select>
-          <button className="edit"> <image src =""></image> </button>
-          <button className="Add">+</button>
+          <button className="edit">✎</button>
+          <button id="add" className="add">+</button>
           <button className="remove">X</button>
         </div>
+        <br></br>
+        <br></br> 
       </div>
     </div>
   );
