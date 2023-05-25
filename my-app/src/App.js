8ea6c147f5e133c87d4e7af82a32fccc32e86015
Zain Ali <zain3002ali@gmail.com>
2023-05-25 18:11:22 +0100

    initial stage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="NavigationBar">
+        <div className="SearchBars">
+          <input></input>
+          {/* drop down menu */}
+        </div>
+        <div className="filters">
+          <input></input>
+          <input></input>
+          {/* drop down menu */}
+          <button className="edit"></button>
+          <button className="Add">+</button>
+          <button className="remove">X</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
 
 // test test test
