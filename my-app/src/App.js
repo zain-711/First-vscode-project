b0d6315fbf13add74c816dd9b71facba6ccdf15a
Zain Ali <zain3002ali@gmail.com>
2023-05-28 19:16:34 +0100

    Search bar componentand seperate css file created.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,53 +1,58 @@
+import React from "react";
 import "./App.css";
-
-
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
-    <div>
-      <div className="NavigationBar">
-        <br></br>
-        <br></br> 
-        <div className="SearchBars">
-          <input className= "search" placeholder = "Search..." defaultValue=""></input>
+    <div className="NavigationBar">
+      <SearchBar></SearchBar>
+      <br></br>
+      <div className="EditBar">
+        <button className="edit">✎</button>
+        <button id="add" className="add">
+          +
+        </button>
 
-          <select id="search-field-choice">
-          <option value="" disabled selected>Select search field</option>
-            <option value="itemId">ID number</option>
-            <option value="itemName">Name</option>
-            <option value="itemPrice">Price</option>
-            <option value="itemCategory">Category</option>
-          </select>
-        </div>
-        <br></br> 
-        <div className="EditBar">
-          <input className="name" placeholder = "enter item name here" defaultValue= ""></input>
-          <input className="price" placeholder = "enter price here" defaultValue=""></input>
-
-          <select id="Categories">
-            <option value="" disabled selected>Please select category</option>
-            <option value="food">Food and Drink</option>
-            <option value="appliance">Appliances</option>
-            <option value="stationary">Stationary</option>
-            <option value="clothes">Clothing</option>
-            <option value="diy">DIY and Repairs</option>
-            <option value="entertainment">Entertainment</option>
-            <option value="garden">Gardening</option>
-            <option value="cleaning">Cleaning Products</option>
-            <option value="toiletries">Toiletries</option>
-            <option value="other">Other Items</option>
-        </select>
-          <button className="edit">✎</button>
-          <button id="add" className="add">+</button>
-
-          <button className="remove">X</button>
-        </div>
-        <br></br>
-        <br></br> 
+        <button className="remove">X</button>
       </div>
+      <br></br>
+      <br></br>
     </div>
   );
 }
 export default App;
 
 // test test test
+
+// TO DO -
+// Seperate button as component.
+// remove second line of inout boxes and keep buttons.
+
+{
+  /* <input
+          className="name"
+          placeholder="enter item name here"
+          defaultValue=""
+        ></input>
+        <input
+          className="price"
+          placeholder="enter price here"
+          defaultValue=""
+        ></input>
+
+        <select id="Categories">
+          <option value="" disabled selected>
+            Please select category
+          </option>
+          <option value="food">Food and Drink</option>
+          <option value="appliance">Appliances</option>
+          <option value="stationary">Stationary</option>
+          <option value="clothes">Clothing</option>
+          <option value="diy">DIY and Repairs</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="garden">Gardening</option>
+          <option value="cleaning">Cleaning Products</option>
+          <option value="toiletries">Toiletries</option>
+          <option value="other">Other Items</option>
+        </select> */
+}
---
+++ my-app/src/SearchBar.js
@@ -0,0 +1,27 @@
+import "./SearchBar.css";
+
+export default function SearchBar() {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <div className="SearchBars">
+        <input
+          className="search"
+          placeholder="Search..."
+          defaultValue=""
+        ></input>
+
+        <select id="search-field-choice">
+          <option value="" disabled selected>
+            Select search field
+          </option>
+          <option value="itemId">ID number</option>
+          <option value="itemName">Name</option>
+          <option value="itemPrice">Price</option>
+          <option value="itemCategory">Category</option>
+        </select>
+      </div>
+    </div>
+  );
+}
