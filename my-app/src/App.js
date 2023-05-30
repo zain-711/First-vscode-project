4925e67ab3c039191c46fde7a33ca2fac2301bd8
Zain Ali <zain3002ali@gmail.com>
2023-05-30 20:03:40 +0100

    restructuring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./SearchBar";
 import Add from "./AddBtn";
 
 function App() {
+  //state array
   return (
     <div>
       <div className="NavigationBar">
@@ -11,16 +12,15 @@ function App() {
         <br></br>
         <br></br>
         <SearchBar></SearchBar>
-        <br></br>
-        <br></br>
-        <br></br>
-        {/* <button className="edit">✎</button> */}
-        {/* <button className="remove">X</button> */}
-      </div>
-      <div className="AddComponent">
         <Add></Add>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
     </div>
   );
 }
 export default App;
+
+// "No reciepts" component
+// if array is empty return "No reciepts."(grey) and "Add a reciept here" (grey) under add button
